be0de872ecc35c5c036685be5cef11e64ad5f2ae
Suparna Khamaru <suparna3107@gmail.com>
2020-06-11 23:17:09 +0530

    power

---
+++ PowerUsingRecursion.swift
@@ -0,0 +1,12 @@
+func power(number: Int, n: Int) -> Int {
+    
+    if n == 0 {  //base case
+        return 1
+    }
+    else {
+        return number * power(number: number, n: n - 1)
+    }
+}
+
+print(power(number: 3, n: 2))         // 9
+print(power(number: 2, n: 3))         // 8
